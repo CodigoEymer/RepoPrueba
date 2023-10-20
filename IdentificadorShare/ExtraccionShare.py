9346d60edda3651c311f48c9a5abc8ebe603bd97
eymerguaquez <eymer_guaquez@eficacia.com.co>
2023-10-20 16:59:46 -0500

    NameShare agregado

--- IdentificadorShare/ExtraccionShare.py
+++ IdentificadorShare/ExtraccionShare.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 codigo = """ """
 
-with open('_Conversor.qvs', 'r') as file:
+with open("_Conversor.qvs", "r", encoding="utf-8") as file:
     codigo = file.read()
 
 # Extraer texto entre "FROM [" y "]"
@@ -13,9 +13,14 @@ Archivo = re.findall(r'FROM\s*\[(.*?)\]', codigo, re.IGNORECASE)                
 
 # Extraer texto entre "table is" y ");"
 #Hoja = re.findall(r'table is (.*?);', codigo)
-Hoja = re.findall(r'table\s*is\s*(.*?);', codigo, re.IGNORECASE)
+#Hoja = re.findall(r'table\s*is\s*(.*?))', codigo)
+Hoja = re.findall(r'table\s*is\s*(.*?)\)', codigo)
 
 Archivo = [a.split('/')[-1] for a in Archivo]
+
+#matches = re.findall(r'Sh_([^.qvd]*)', codigo)
+#matches = re.findall(r'(Sh_[^.qvd]*)', codigo)
+NameShare = re.findall(r'(Sh_.*?).qvd', codigo)
 
 #for i in range(len(Archivo)):
     #print("Archivo:"+Archivo[i]+"   Hoja: "+ Hoja[i])
@@ -29,7 +34,8 @@ elif len(Hoja) > len(Archivo):
 # Crear un DataFrame de pandas con las dos listas
 df = pd.DataFrame({
     'Archivo': Archivo,
-    'Hoja': Hoja
+    'Hoja': Hoja,
+    'NameShare': NameShare
 })
 
 # Guardar el DataFrame en un archivo Excel
